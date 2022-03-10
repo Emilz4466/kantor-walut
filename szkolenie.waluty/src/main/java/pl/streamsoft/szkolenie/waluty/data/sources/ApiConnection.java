8af18b13d8d4887b37d8f3191f8b6985c9434b8d
emil.zyberski <emil.zyberski@STREAMSOFT-0177.streamsoft.pl>
2022-03-10 18:19:35 +0100

    Pobranie danych z api + urlBuilder

--- szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/sources/ApiConnection.java
+++ szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/sources/ApiConnection.java
@@ -1,5 +1,88 @@
 package pl.streamsoft.szkolenie.waluty.data.sources;
 
-public class ApiConnection {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
 
+import pl.streamsoft.szkolenie.waluty.data.sources.url.Url;
+import pl.streamsoft.szkolenie.waluty.data.sources.url.UrlBuilder;
+
+public class ApiConnection {
+	
+	private Url url;
+	private UrlBuilder urlBuilder;
+	
+	
+	
+	ApiConnection(Url url) {
+		this.url = url;
+		dateValidator();
+	}
+	
+	private Boolean isDataAvailable() {
+		
+		try {
+			URL urlConnector = new URL(url.getUrl());
+			HttpURLConnection connection;
+			connection = (HttpURLConnection) urlConnector.openConnection();
+			connection.setRequestMethod("GET");
+			return true;
+		
+		} catch (IOException e) {
+			return false;
+		}
+		
+	}
+	
+	private void dateValidator() {
+		LocalDate givenDate = url.getDate();
+		LocalDate lastCurrency = LocalDate.of(2002, 01, 02);
+		
+		if(givenDate.isBefore(lastCurrency)) {
+			givenDate = lastCurrency;
+		}
+		else if(givenDate.isBefore(LocalDate.now())) {
+			givenDate = LocalDate.now();
+		}
+		
+		urlBuilder.setDate(givenDate);
+		urlBuilder.setCurrency(url.getCurrency());
+		urlBuilder.setFormat(url.getFormat());
+		
+		while(!isDataAvailable()) {
+			givenDate = givenDate.minusDays(1);
+			urlBuilder.setDate(givenDate);
+			url = urlBuilder.buildUrl();
+		}
+	}
+	
+	private StringBuffer response() throws IOException {
+		
+		URL urlConnector = new URL(url.getUrl());
+		HttpURLConnection connection;
+		connection = (HttpURLConnection) urlConnector.openConnection();
+		connection.setRequestMethod("GET");
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		return response;
+	}
+	
+	public String getResponse() {
+		try {
+			return response().toString();
+		} catch (IOException e) {
+			return null;
+		}
+	}
 }
--- szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/sources/url/Url.java
+++ szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/sources/url/Url.java
@@ -1,5 +1,31 @@
 package pl.streamsoft.szkolenie.waluty.data.sources.url;
 
-public class Url {
+import java.time.LocalDate;
 
+import pl.streamsoft.szkolenie.waluty.data.Currency;
+
+public class Url {
+	private final Currency currency;
+	private final LocalDate date;
+	private final String format;
+	
+	public Url(Currency currency, LocalDate date, String format) {
+		this.currency = currency;
+		this.date = date;
+		this.format = format;
+	}
+	
+	public Currency getCurrency() {
+		return currency;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public String getFormat() {
+		return format;
+	}
+	
+	public String getUrl() {
+		return "https://api.nbp.pl/api/exchangerates/rates/a/" + this.currency + "/" + this.date + "?format=" + this.format;
+	}
 }
