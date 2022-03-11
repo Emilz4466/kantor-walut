c501bea57f595ef3975cf636e77ad35567cca194
emil.zyberski <emil.zyberski@STREAMSOFT-0177.streamsoft.pl>
2022-03-11 10:15:48 +0100

    Obiekty Api

--- szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/objects/Rate.java
+++ szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/objects/Rate.java
@@ -1,5 +1,27 @@
 package pl.streamsoft.szkolenie.waluty.data.objects;
 
-public class Rate {
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+
+
+public class Rate {
+	@JacksonXmlElementWrapper(localName = "No")
+	private String no = null;
+	@JacksonXmlElementWrapper(localName = "EffectiveDate")
+	private LocalDate effectiveDate = null;
+	@JacksonXmlElementWrapper(localName = "Mid")
+	private BigDecimal mid = null;
+	
+	public String getNo() {
+		return no;
+	}
+	public LocalDate getEffectiveDate() {
+		return effectiveDate;
+	}
+	public BigDecimal getMid() {
+		return mid;
+	}
+	
 }
