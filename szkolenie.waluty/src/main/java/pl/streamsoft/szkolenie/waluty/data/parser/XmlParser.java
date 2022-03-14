a309c98ee56f3aa6d1757c48ecc42acc9331f939
emil.zyberski <emil.zyberski@STREAMSOFT-0177.streamsoft.pl>
2022-03-14 15:52:01 +0100

    XmlParser

--- szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/parser/XmlParser.java
+++ szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/parser/XmlParser.java
@@ -1,5 +1,40 @@
 package pl.streamsoft.szkolenie.waluty.data.parser;
 
-public class XmlParser {
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import pl.streamsoft.szkolenie.waluty.data.objects.ExchangeRatesSeries;
+import pl.streamsoft.szkolenie.waluty.data.sources.ResponseStrategy;
+
+public class XmlParser implements ParserStrategy{
+	
+	private ResponseStrategy response;
+	private ExchangeRatesSeries exchangeRatesSeries = new ExchangeRatesSeries();
+
+	private void getObjectFromApi() {
+
+		XmlMapper mapper = new XmlMapper();
+				
+		try {
+			this.exchangeRatesSeries = mapper.readValue(response.getResponse(), ExchangeRatesSeries.class);
+		} catch (JsonProcessingException e) {
+			this.exchangeRatesSeries = null;
+		}
+		
+	}
+	
+	@Override
+	public String getFormat() {
+		return "xml";
+	}
+
+	@Override
+	public BigDecimal getRate(ResponseStrategy response) {
+		this.response = response;
+		getObjectFromApi();
+		return exchangeRatesSeries.getRates().get(0).getMid();	
+	}
 
 }
