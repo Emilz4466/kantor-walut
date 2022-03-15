be2e7ce83c542b1d10ed08324ff18cc0a816fff5
emil.zyberski <emil.zyberski@STREAMSOFT-0177.streamsoft.pl>
2022-03-15 15:46:06 +0100

    Settery w url

--- szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/sources/url/Url.java
+++ szkolenie.waluty/src/main/java/pl/streamsoft/szkolenie/waluty/data/sources/url/Url.java
@@ -5,27 +5,42 @@ import java.time.LocalDate;
 import pl.streamsoft.szkolenie.waluty.data.Currency;
 
 public class Url {
-	private final Currency currency;
-	private final LocalDate date;
-	private final String format;
-	
+	private Currency currency;
+	private LocalDate date;
+	private String format;
+
 	public Url(Currency currency, LocalDate date, String format) {
 		this.currency = currency;
 		this.date = date;
 		this.format = format;
 	}
-	
+
 	public Currency getCurrency() {
 		return currency;
 	}
+
 	public LocalDate getDate() {
 		return date;
 	}
+
 	public String getFormat() {
 		return format;
 	}
-	
+
 	public String getUrl() {
-		return "https://api.nbp.pl/api/exchangerates/rates/a/" + this.currency.getCode() + "/" + this.date + "/?format=" + this.format;
+		return "https://api.nbp.pl/api/exchangerates/rates/a/" + this.currency.getCode() + "/" + this.date + "/?format="
+				+ this.format;
+	}
+
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
 	}
 }
